5477811cc277b3e1f558006c2db2b9851d55a461
Lorenzo <lorenzo.carbonell.cerezo@gmail.com>
2022-01-02 09:30:41 +0100

    update download

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,185 @@
-use std::num;
-
 use clap::Parser;
-use roxmltree::{Document, Node};
+use std::sync::mpsc::channel;
+use std::thread;
+use spinners::{Spinner, Spinners};
+use roxmltree::Document;
 
+/*
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 const NAME: &str = env!("CARGO_PKG_NAME");
 const AUTHORS: &str = env!("CARGO_PKG_AUTHORS");
+*/
 
 struct Item{
     title: String,
     description: String,
-    link: String
+    enclosure: String,
+    link: String,
+    image: String,
+}
+
+impl Item{
+    fn new(title: &str, description: &str, enclosure: &str, link: &str, image: &str)->Item{
+        Self{title: title.to_string(),
+             description: description.to_string(),
+             enclosure: enclosure.to_string(),
+             link: link.to_string(),
+             image: image.to_string()}
+    }
+    fn print(&self){
+        println!("Title: {}", self.title);
+        println!("Description: {}", self.description);
+        println!("Enclosure: {}", self.enclosure);
+        println!("Link: {}", self.link);
+        println!("Image: {}", self.image);
+    }
 }
 
 #[derive(Parser, Debug)]
 #[clap(about, version, author)]
 struct Args {
-    /// Name of the person to greet
-    #[clap(short, long)]
-    name: String,
+    /// Debug mode
+    #[clap(short='D', long)]
+    debug: bool,
 
-    /// Number of times to greet
-    #[clap(short, long, default_value_t = 1)]
-    count: u8,
+    /// Show the title of the episode(s)
+    #[clap(short, long)]
+    title: bool,
+
+    /// Show the description of the episode(s)
+    #[clap(short, long)]
+    description: bool,
+
+    /// The url of the feed of the podcast. Required.
+    #[clap(short, long)]
+    url: String,
+
+    /// The first episode to adquire. If none, default is the first.
+    #[clap(short='F', long)]
+    first: Option<i32>,
+
+    /// The last episode to adquire. If none, default is the last.
+    #[clap(short='L', long)]
+    last: Option<i32>,
+
+    /// Download the picture of the episode(s)
+    #[clap(short, long)]
+    image: bool,
+
+    /// Download the audio of the episode(s)
+    #[clap(short, long)]
+    audio: bool,
+
+    /// Download the feed
+    #[clap(short, long)]
+    feed: bool,
+
+    /// Play the audio of the episode(s)
+    #[clap(short, long)]
+    play: bool,
+
 }
 
 fn main() {
     let args = Args::parse();
-
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name)
+    let url = args.url;
+    let first: usize;
+    let mut last: usize;
+    let (sender, receiver) = channel();
+    let spinner = Spinner::new(&Spinners::Dots9,
+                               "Downloading feed".to_string());
+    let thread_join_handle = thread::spawn(move || {
+        let items = get_rss(&url);
+        let _ = sender.send(items);
+    });
+    let _ = thread_join_handle.join();
+    spinner.stop();
+    let feed = receiver.recv().unwrap();
+    let noi: i32 = feed.len().try_into().unwrap();
+    if let Some(value) = args.first{
+        if value <= 0 && value <= noi {
+            first = (noi - value).try_into().unwrap();
+        }else if value < 0 && value > noi {
+            first = 0;
+        } else {
+            first = (value - 1).try_into().unwrap();
+        }
+    }else{
+        first = 0;
+    }
+    if let Some(value) = args.last{
+        if value <= 0 && value < noi {
+            last = (noi - value).try_into().unwrap();
+        }else if value <= 0 && value > noi {
+            last = (noi - 1).try_into().unwrap();
+        } else {
+            last = (value - 1).try_into().unwrap();
+        }
+    }else{
+        last = (noi - 1).try_into().unwrap();
+    }
+    if last < first{
+        last = first;
+    }
+    for item in feed.iter().take(last + 1).skip(first){
+        item.print();
     }
 }
 
+
 #[test]
-fn get_rss(){
-    let body = reqwest::blocking::get("https://atareao.es/mp3-feed/").unwrap().text().unwrap();
-    //println!("body = {:?}", body);
-    let document = Document::parse(&body).unwrap();
-    let rss = document.root().first_child().unwrap();
-    for child in rss.children(){
-        println!("Child name: {}", child.tag_name().name());
-    }
-    let channel = rss.children().find(|i| i.has_tag_name("channel")).unwrap();
-    println!("Channel: '{}'", channel.tag_name().name());
-    for item in channel.children().filter(|i| i.has_tag_name("item")).into_iter(){
-        let title = item.children().find(|p| p.has_tag_name("title")).unwrap().text().unwrap();
-        let description = item.children().find(|p| p.has_tag_name("description")).unwrap().text().unwrap();
-        let mut en: &str;
-        if let Some(numero) = item.children().find(|p| p.has_tag_name("episode")){
-            en = numero.text().unwrap();
-        }else{
-            en = "Unknown";
-        }
-        println!("El título del episodio {} es: {}", en, title);
-    }
+fn test_get_rss(){
     let temporal = "!";
     assert_eq!(temporal, "");
+}
+fn get_rss(url: &str)->Vec<Item>{
+    let mut items: Vec<Item> = Vec::new();
+    let body = reqwest::blocking::get(url).unwrap().text().unwrap();
+    let document = Document::parse(&body).unwrap();
+    let rss = document.root().first_child().unwrap();
+    let channel = rss.children().find(|i| i.has_tag_name("channel")).unwrap();
+    for item in channel.children().filter(|i| i.has_tag_name("item")).into_iter(){
+        let title;
+        let description;
+        let enclosure;
+        let link;
+        let image;
+        if let Some(value) = item.children().find(|p| p.has_tag_name("title")){
+            if let Some(text) = value.text(){ title = text; }else{ title = ""; }
+        }else{
+            title = "";
+        }
+        if let Some(value) = item.children().find(|p| p.has_tag_name("description")){
+            if let Some(text) = value.text(){ description = text; }else{ description = ""; }
+        }else{
+            description = "";
+        }
+        if let Some(value) = item.children().find(|p| p.has_tag_name("enclosure")){
+            if let Some(attribute) = value.attributes().iter().find(|a| a.name() == "url"){
+                enclosure = attribute.value();
+            }else{
+                enclosure = "";
+            }
+        }else{
+            enclosure = "";
+        }
+        if let Some(value) = item.children().find(|p| p.has_tag_name("link")){
+            if let Some(text) = value.text(){ link = text; }else{ link = ""; }
+        }else{
+            link = "";
+        }
+        if let Some(value) = item.children().find(|p| p.has_tag_name("image")){
+            if let Some(attribute) = value.attributes().iter().find(|a| a.name() == "href"){
+                image = attribute.value();
+            }else{
+                image = "";
+            }
+        }else{
+            image = "";
+        }
+        let item = Item::new(title, description, enclosure, link, image);
+        items.push(item);
+    }
+    items.reverse();
+    items
 }
