403d4292d68f5165dd0269bab43c89e11ce63a48
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-08-29 20:03:45 +0200

    makefile

--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,99 @@
 mod podcast;
 
-use std::process;
 use crate::podcast::Podcast;
+use std::process;
 
-use clap::{App, Arg, AppSettings};
-use spinners::{Spinner, Spinners};
 use crate::podcast::get_rss;
-use inquire::{Text, Select};
+use clap::{App, AppSettings, Arg};
 use colored::*;
+use inquire::{Select, Text};
+use itertools::Itertools;
 use regex::Regex;
+use rodio::{source::Source, Decoder, OutputStream, Sink};
+use spinners::{Spinner, Spinners};
 use std::fs::File;
 use std::io::BufReader;
-use rodio::{Decoder, OutputStream, source::Source, Sink};
-use itertools::Itertools;
 
-const NAME: &str =env!("CARGO_PKG_NAME");
-const DESCRIPTION: &str =env!("CARGO_PKG_DESCRIPTION");
+const NAME: &str = env!("CARGO_PKG_NAME");
+const DESCRIPTION: &str = env!("CARGO_PKG_DESCRIPTION");
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 const AUTHORS: &str = env!("CARGO_PKG_AUTHORS");
 
-
 #[tokio::main]
-async fn main(){
+async fn main() {
     let matches = App::new(NAME)
         .version(VERSION)
-       .author(AUTHORS)
+        .author(AUTHORS)
         .about(DESCRIPTION)
         .setting(AppSettings::ArgRequiredElseHelp)
-        .arg(Arg::new("debug")
-             .short('d')
-             .long("debug")
-             .takes_value(false))
-        .arg(Arg::new("interactive")
-             .short('i')
-             .long("interactive")
-             .takes_value(false))
-        .arg(Arg::new("url")
-             .short('u')
-             .long("url")
-             .required(true)
-             .takes_value(true))
-        .subcommand(App::new("list")
-                    .about("List")
-                    .arg(Arg::new("first")
-                         .short('f')
-                         .long("first")
-                         .required(false)
-                         .takes_value(true))
-                    .arg(Arg::new("last")
-                         .short('l')
-                         .long("last")
-                         .required(false)
-                         .takes_value(true))
-                    .arg(Arg::new("json")
-                         .short('j')
-                         .long("json")
-                         .takes_value(false))
-                    )
+        .arg(
+            Arg::new("debug")
+                .short('d')
+                .long("debug")
+                .takes_value(false),
+        )
+        .arg(
+            Arg::new("interactive")
+                .short('i')
+                .long("interactive")
+                .takes_value(false),
+        )
+        .arg(
+            Arg::new("url")
+                .short('u')
+                .long("url")
+                .required(true)
+                .takes_value(true),
+        )
+        .subcommand(
+            App::new("list")
+                .about("List")
+                .arg(
+                    Arg::new("first")
+                        .short('f')
+                        .long("first")
+                        .required(false)
+                        .takes_value(true),
+                )
+                .arg(
+                    Arg::new("last")
+                        .short('l')
+                        .long("last")
+                        .required(false)
+                        .takes_value(true),
+                )
+                .arg(Arg::new("json").short('j').long("json").takes_value(false)),
+        )
         .get_matches();
     let url = matches.value_of("url").unwrap();
-    if matches.is_present("interactive"){
-        let spinner = Spinner::new(&Spinners::Dots9,
-                                   "Downloading feed".to_string());
+    if matches.is_present("interactive") {
+        let spinner = Spinner::new(&Spinners::Dots9, "Downloading feed".to_string());
         let podcast = get_rss(url).await.unwrap();
         spinner.stop();
-        loop{
+        loop {
             interactive(&podcast).await;
         }
-    }else if let Some(sub) = matches.subcommand_matches("list"){
+    } else if let Some(sub) = matches.subcommand_matches("list") {
         let url = sub.value_of("url").unwrap();
 
-        let spinner = Spinner::new(&Spinners::Dots9,
-                                   "Downloading feed".to_string());
+        let spinner = Spinner::new(&Spinners::Dots9, "Downloading feed".to_string());
         let podcast = get_rss(url).await.unwrap();
         let items = podcast.get_episodes();
         spinner.stop();
-        for id in items.keys(){
-            println!("{} {} {}",
-                     "=====".cyan(),
-                     id.to_string().blue(),
-                     "=====".cyan());
+        for id in items.keys() {
+            println!(
+                "{} {} {}",
+                "=====".cyan(),
+                id.to_string().blue(),
+                "=====".cyan()
+            );
             items.get(id).unwrap().print();
         }
         process::exit(0);
     }
 }
 
-fn play(filename: &str){
+fn play(filename: &str) {
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
     let file = BufReader::new(File::open(filename).unwrap());
     let source = Decoder::new(file).unwrap();
@@ -95,63 +102,74 @@ fn play(filename: &str){
     sink.sleep_until_end();
 }
 
-async fn interactive(podcast: &Podcast){
-    let options = vec!["1. List episodes", "2. Get episode", "3. Play episode", "4. Exit"];
+async fn interactive(podcast: &Podcast) {
+    let options = vec![
+        "1. List episodes",
+        "2. Get episode",
+        "3. Play episode",
+        "4. Exit",
+    ];
     let ans = Select::new("Select option:", options).prompt();
     match ans {
         Ok(choice) => {
             println!("Selected {}", choice);
-            if choice.contains("List episodes"){
-                for id in podcast.get_episodes().keys().sorted(){
+            if choice.contains("List episodes") {
+                for id in podcast.get_episodes().keys().sorted() {
                     let episode = podcast.get_episodes().get(id).unwrap();
                     println!("{}", episode);
                 }
-            }else if choice.contains("Exit") {
+            } else if choice.contains("Exit") {
                 process::exit(0);
-            }else if choice.contains("Get episode") {
+            } else if choice.contains("Get episode") {
                 let ans = Select::new("Select option:", podcast.get_titles()).prompt();
-                match ans{
+                match ans {
                     Ok(choice) => {
                         println!("{}", &choice);
                         let re = Regex::new(r"(\d*)\.").unwrap();
                         let capture = re.captures(&choice).unwrap();
-                        let id = capture.get(1).map_or("", |m| m.as_str()).parse::<usize>().unwrap();
+                        let id = capture
+                            .get(1)
+                            .map_or("", |m| m.as_str())
+                            .parse::<usize>()
+                            .unwrap();
                         let episode = podcast.get_episodes().get(&id).unwrap();
                         episode.print();
-                    },
+                    }
                     Err(_) => println!("There was an error, please select again"),
                 }
-            }else if choice.contains("Play episode") {
+            } else if choice.contains("Play episode") {
                 let ans = Select::new("Select option:", podcast.get_titles()).prompt();
-                match ans{
+                match ans {
                     Ok(choice) => {
                         println!("{}", &choice);
                         let re = Regex::new(r"(\d*)\.").unwrap();
                         let capture = re.captures(&choice).unwrap();
-                        let id = capture.get(1).map_or("", |m| m.as_str()).parse::<usize>().unwrap();
+                        let id = capture
+                            .get(1)
+                            .map_or("", |m| m.as_str())
+                            .parse::<usize>()
+                            .unwrap();
                         let episode = podcast.get_episodes().get(&id).unwrap();
                         episode.print();
-                        let spinner = Spinner::new(&Spinners::Dots9,
-                                                   "Downloading episode".to_string());
+                        let spinner =
+                            Spinner::new(&Spinners::Dots9, "Downloading episode".to_string());
                         let filename = format!("/tmp/{}.mp3", id);
                         println!("{:?}", &filename);
                         episode.download(&filename).await;
                         spinner.message("Playing...".to_string());
                         play(&filename);
                         spinner.stop();
-                    },
+                    }
                     Err(_) => println!("There was an error, please select again"),
                 }
             }
-        },
-        Err(_) => println!("There was an error, please select again"),
         }
-
+        Err(_) => println!("There was an error, please select again"),
+    }
 }
 
-
 #[test]
-fn test_get_rss(){
+fn test_get_rss() {
     let temporal = "!";
     assert_eq!(temporal, "");
 }
--- src/podcast.rs
+++ src/podcast.rs
@@ -1,12 +1,12 @@
-use roxmltree::Document;
 use colored::*;
-use reqwest::Error;
-use std::{fmt, io::Cursor, fs::File, collections::HashMap};
-use itertools::Itertools;
 use html2md::parse_html;
+use itertools::Itertools;
+use reqwest::Error;
+use roxmltree::Document;
+use std::{collections::HashMap, fmt, fs::File, io::Cursor};
 use termimad::{self, MadSkin};
 
-pub struct Episode{
+pub struct Episode {
     id: usize,
     title: String,
     description: String,
@@ -15,13 +15,13 @@ pub struct Episode{
     image: String,
 }
 
-impl fmt::Display for Episode{
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result{
+impl fmt::Display for Episode {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}. {}", self.id, self.title)
     }
 }
 
-pub struct Podcast{
+pub struct Podcast {
     url: String,
     title: String,
     description: String,
@@ -30,33 +30,29 @@ pub struct Podcast{
     episodes: HashMap<usize, Episode>,
 }
 
-impl Podcast{
-    pub async fn new(url: &str)->Result<Podcast, Error>{
+impl Podcast {
+    pub async fn new(url: &str) -> Result<Podcast, Error> {
         get_rss(url).await
     }
 
-    pub fn get_episodes(&self)->&HashMap<usize, Episode>{
+    pub fn get_episodes(&self) -> &HashMap<usize, Episode> {
         &self.episodes
     }
 
-    pub fn get_title(&self)->&str{
+    pub fn get_title(&self) -> &str {
         &self.title
     }
 
-    pub fn get_description(&self)->&str{
+    pub fn get_description(&self) -> &str {
         &self.description
     }
 
-    async fn get_raw_rss(url: &str) -> Result<String, Error>{
-        return reqwest::get(url)
-            .await
-            .unwrap()
-            .text()
-            .await
+    async fn get_raw_rss(url: &str) -> Result<String, Error> {
+        return reqwest::get(url).await.unwrap().text().await;
     }
 
-    pub fn get_titles(&self) -> Vec<String>{
-        let mut ans:Vec<String> = Vec::new();
+    pub fn get_titles(&self) -> Vec<String> {
+        let mut ans: Vec<String> = Vec::new();
         for id in self.episodes.keys().sorted().rev() {
             let episode = self.episodes.get(id).unwrap();
             ans.push(episode.to_string())
@@ -65,19 +61,14 @@ impl Podcast{
     }
 }
 
-pub async fn get_rss(url: &str)->Result<Podcast, Error>{
+pub async fn get_rss(url: &str) -> Result<Podcast, Error> {
     let url = url.to_string();
     let mut description = "".to_string();
     let mut title = "".to_string();
     let mut link = "".to_string();
     let mut image = "".to_string();
     let mut episodes: HashMap<usize, Episode> = HashMap::new();
-    let body = reqwest::get(&url)
-        .await
-        .unwrap()
-        .text()
-        .await
-        .unwrap();
+    let body = reqwest::get(&url).await.unwrap().text().await.unwrap();
     let document = Document::parse(&body).unwrap();
     let rss = document.root().first_child().unwrap();
     let channel = rss.children().find(|i| i.has_tag_name("channel")).unwrap();
@@ -87,77 +78,85 @@ pub async fn get_rss(url: &str)->Result<Podcast, Error>{
      * link
      * image / url
      */
-    if let Some(node) = channel.children().find(|p| p.has_tag_name("description")){
-        if let Some(text) = node.text(){
+    if let Some(node) = channel.children().find(|p| p.has_tag_name("description")) {
+        if let Some(text) = node.text() {
             description = parse_html(text);
         }
     }
-    if let Some(node) = channel.children().find(|p| p.has_tag_name("title")){
-        if let Some(text) = node.text(){
+    if let Some(node) = channel.children().find(|p| p.has_tag_name("title")) {
+        if let Some(text) = node.text() {
             title = text.to_string();
         }
     }
-    if let Some(node) = channel.children().find(|p| p.has_tag_name("link")){
-        if let Some(text) = node.text(){
+    if let Some(node) = channel.children().find(|p| p.has_tag_name("link")) {
+        if let Some(text) = node.text() {
             link = text.to_string();
         }
     }
-    if let Some(node) = channel.children().find(|p| p.has_tag_name("image")){
-        if let Some(text) = node.text(){
+    if let Some(node) = channel.children().find(|p| p.has_tag_name("image")) {
+        if let Some(text) = node.text() {
             image = text.to_string();
         }
     }
     let chapters = channel.children().filter(|i| i.has_tag_name("item"));
     let total = chapters.clone().count();
-    for (index, item) in chapters.enumerate(){
+    for (index, item) in chapters.enumerate() {
         let id = total - index;
         let title;
         let description;
         let enclosure;
         let link;
         let image;
-        if let Some(value) = item.children().find(|p| p.has_tag_name("title")){
-            if let Some(text) = value.text(){ title = text; }else{ title = ""; }
-        }else{
+        if let Some(value) = item.children().find(|p| p.has_tag_name("title")) {
+            if let Some(text) = value.text() {
+                title = text;
+            } else {
+                title = "";
+            }
+        } else {
             title = "";
         }
-        if let Some(value) = item.children().find(|p| p.has_tag_name("description")){
-            if let Some(text) = value.text(){
+        if let Some(value) = item.children().find(|p| p.has_tag_name("description")) {
+            if let Some(text) = value.text() {
                 description = parse_html(text);
-            }else{
+            } else {
                 description = "".to_string();
             }
-        }else{
+        } else {
             description = "".to_string();
         }
-        if let Some(value) = item.children().find(|p| p.has_tag_name("enclosure")){
-            if let Some(attribute) = value.attributes().iter().find(|a| a.name() == "url"){
+        if let Some(value) = item.children().find(|p| p.has_tag_name("enclosure")) {
+            if let Some(attribute) = value.attributes().iter().find(|a| a.name() == "url") {
                 enclosure = attribute.value();
-            }else{
+            } else {
                 enclosure = "";
             }
-        }else{
+        } else {
             enclosure = "";
         }
-        if let Some(value) = item.children().find(|p| p.has_tag_name("link")){
-            if let Some(text) = value.text(){ link = text; }else{ link = ""; }
-        }else{
+        if let Some(value) = item.children().find(|p| p.has_tag_name("link")) {
+            if let Some(text) = value.text() {
+                link = text;
+            } else {
+                link = "";
+            }
+        } else {
             link = "";
         }
-        if let Some(value) = item.children().find(|p| p.has_tag_name("image")){
-            if let Some(attribute) = value.attributes().iter().find(|a| a.name() == "href"){
+        if let Some(value) = item.children().find(|p| p.has_tag_name("image")) {
+            if let Some(attribute) = value.attributes().iter().find(|a| a.name() == "href") {
                 image = attribute.value();
-            }else{
+            } else {
                 image = "";
             }
-        }else{
+        } else {
             image = "";
         }
         println!("{} - {}", id, title);
         let episode = Episode::new(id, title, &description, enclosure, link, image);
         episodes.insert(id, episode);
     }
-    Ok(Podcast{
+    Ok(Podcast {
         url,
         title,
         description,
@@ -167,38 +166,46 @@ pub async fn get_rss(url: &str)->Result<Podcast, Error>{
     })
 }
 
-impl Episode{
-    pub fn new(id: usize, title: &str, description: &str, enclosure: &str, link: &str,
-               image: &str)->Self{
-        Self{id,
-             title: title.to_string(),
-             description: description.to_string(),
-             enclosure: enclosure.to_string(),
-             link: link.to_string(),
-             image: image.to_string()}
+impl Episode {
+    pub fn new(
+        id: usize,
+        title: &str,
+        description: &str,
+        enclosure: &str,
+        link: &str,
+        image: &str,
+    ) -> Self {
+        Self {
+            id,
+            title: title.to_string(),
+            description: description.to_string(),
+            enclosure: enclosure.to_string(),
+            link: link.to_string(),
+            image: image.to_string(),
+        }
     }
 
-    pub fn get_id(&self)->&usize{
+    pub fn get_id(&self) -> &usize {
         &self.id
     }
 
-    pub fn get_title(&self)->&str{
+    pub fn get_title(&self) -> &str {
         &self.title
     }
 
-    pub fn get_descrption(&self)->&str{
+    pub fn get_descrption(&self) -> &str {
         &self.description
     }
 
-    pub fn get_enclosure(&self)->&str{
+    pub fn get_enclosure(&self) -> &str {
         &self.enclosure
     }
 
-    pub fn get_link(&self)->&str{
+    pub fn get_link(&self) -> &str {
         &self.link
     }
 
-    pub fn print(&self){
+    pub fn print(&self) {
         println!("{}: {}", "Title".red(), self.title.blue());
         println!("{}:", "Description".red());
 
@@ -212,7 +219,7 @@ impl Episode{
         println!("{}: {}", "Image".red(), self.image);
     }
 
-    pub async fn download(&self, filename: &str) -> Result<bool, Error>{
+    pub async fn download(&self, filename: &str) -> Result<bool, Error> {
         let response = reqwest::get(&self.enclosure).await?;
         let mut content = Cursor::new(response.bytes().await?);
         let mut file = File::create(&filename).unwrap();
@@ -221,4 +228,3 @@ impl Episode{
         Ok(true)
     }
 }
-
