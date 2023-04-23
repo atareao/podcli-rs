0f8154db3186e9900e70842aa71cb8021d8f8228
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2023-04-23 08:22:58 +0200

    clap with derive

--- src/main.rs
+++ src/main.rs
@@ -4,93 +4,93 @@ use crate::podcast::Podcast;
 use std::process;
 
 use crate::podcast::get_rss;
-use clap::{App, AppSettings, Arg};
 use colored::*;
-use inquire::{Select, Text};
+use inquire::Select;
 use itertools::Itertools;
 use regex::Regex;
-use rodio::{source::Source, Decoder, OutputStream, Sink};
+use rodio::{Decoder, OutputStream, Sink};
 use spinners::{Spinner, Spinners};
 use std::fs::File;
 use std::io::BufReader;
+use clap::{Parser, Args, Subcommand, ArgAction};
 
-const NAME: &str = env!("CARGO_PKG_NAME");
 const DESCRIPTION: &str = env!("CARGO_PKG_DESCRIPTION");
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 const AUTHORS: &str = env!("CARGO_PKG_AUTHORS");
 
+#[derive(Parser)]
+#[command(author = AUTHORS, version = VERSION, about, long_about = None)]
+struct Cli {
+    #[arg(short, long, action = ArgAction::SetTrue)]
+    /// debug podcli
+    debug: bool,
+
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Adds files to myapp
+    List(ListArgs),
+    /// Run interactive mode
+    Interactive(InteractiveArgs),
+}
+
+#[derive(Args)]
+struct ListArgs{
+    #[arg(short, long)]
+    url: String,
+
+    #[arg(short, long)]
+    first: Option<String>,
+
+    #[arg(short, long)]
+    last: Option<String>,
+
+    #[arg(short, long)]
+    json: Option<bool>,
+
+}
+
+#[derive(Args)]
+struct InteractiveArgs{
+    #[arg(short, long)]
+    url: String,
+}
+
 #[tokio::main]
 async fn main() {
-    let matches = App::new(NAME)
-        .version(VERSION)
-        .author(AUTHORS)
-        .about(DESCRIPTION)
-        .setting(AppSettings::ArgRequiredElseHelp)
-        .arg(
-            Arg::new("debug")
-                .short('d')
-                .long("debug")
-                .takes_value(false),
-        )
-        .arg(
-            Arg::new("interactive")
-                .short('i')
-                .long("interactive")
-                .takes_value(false),
-        )
-        .arg(
-            Arg::new("url")
-                .short('u')
-                .long("url")
-                .required(true)
-                .takes_value(true),
-        )
-        .subcommand(
-            App::new("list")
-                .about("List")
-                .arg(
-                    Arg::new("first")
-                        .short('f')
-                        .long("first")
-                        .required(false)
-                        .takes_value(true),
-                )
-                .arg(
-                    Arg::new("last")
-                        .short('l')
-                        .long("last")
-                        .required(false)
-                        .takes_value(true),
-                )
-                .arg(Arg::new("json").short('j').long("json").takes_value(false)),
-        )
-        .get_matches();
-    let url = matches.value_of("url").unwrap();
-    if matches.is_present("interactive") {
-        let mut spinner = Spinner::new(Spinners::Dots9, "Downloading feed".to_string());
-        let podcast = get_rss(url).await.unwrap();
-        spinner.stop();
-        loop {
-            interactive(&podcast).await;
-        }
-    } else if let Some(sub) = matches.subcommand_matches("list") {
-        let url = sub.value_of("url").unwrap();
+    let cli = Cli::parse();
+    match &cli.command{
+        Commands::List(args) => {
+            let url = &args.url;
+            let mut spinner = Spinner::new(Spinners::Dots9, "Downloading feed".to_string());
+            let podcast = get_rss(url).await.unwrap();
+            let items = podcast.get_episodes();
+            spinner.stop();
+            for id in items.keys() {
+                println!(
+                    "{} {} {}",
+                    "=====".cyan(),
+                    id.to_string().blue(),
+                    "=====".cyan()
+                );
+                items.get(id).unwrap().print();
+            }
+        },
+        Commands::Interactive(args) =>{
+            let url = &args.url;
+            let mut spinner = Spinner::new(Spinners::Dots9, "Downloading feed".to_string());
+            let podcast = get_rss(url).await.unwrap();
+            spinner.stop();
+            loop {
+                interactive(&podcast).await;
+            }
 
-        let mut spinner = Spinner::new(Spinners::Dots9, "Downloading feed".to_string());
-        let podcast = get_rss(url).await.unwrap();
-        let items = podcast.get_episodes();
-        spinner.stop();
-        for id in items.keys() {
-            println!(
-                "{} {} {}",
-                "=====".cyan(),
-                id.to_string().blue(),
-                "=====".cyan()
-            );
-            items.get(id).unwrap().print();
         }
-        process::exit(0);
     }
+    process::exit(0);
 }
 
 fn play(filename: &str) {
